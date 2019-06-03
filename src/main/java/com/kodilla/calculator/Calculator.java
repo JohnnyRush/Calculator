2ff3dfbc5eba0eb53855309b34ee09a1fc1bb9f7
JohnnyRush <nasz_email_do_github>
2019-06-03 18:38:00 +0200

    Calculator

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.calculator;
+
+public class Calculator {
+
+    public void addition(){}
+    public void substraction(){}
+
+    public static void main(String args[]){
+        System.out.println("it works!!");
+    }
+}
